a1994c42969996400b2b61bfe42ef0086e544f4a
Yujaehyeong <mynameisyjh@gmail.com>
2019-11-15 23:42:41 +0900

    index.html파일 template 바로 하위로 이동

--- src/main/java/com/example/practice/controller/TestController.java
+++ src/main/java/com/example/practice/controller/TestController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/test")
 public class TestController {
 
-    @RequestMapping("/test") // localhostL8088/test/test로 접근
-    public String getRequest(Model model){ // Model 객체 파라미터 추가
-        // Model 객체에 데이터 저장 - key:"greeting" | value:"Hello, world!"
-        model.addAttribute("greeting", "Hello, world!");
-        return "index/index"; // index.html 파일 경로매핑
-    }
+//    @RequestMapping("/test") // localhostL8088/test/test로 접근
+//    public String getRequest(Model model){ // Model 객체 파라미터 추가
+//        // Model 객체에 데이터 저장 - key:"greeting" | value:"Hello, world!"
+//        model.addAttribute("greeting", "Hello, world!");
+//        return "index/index"; // index.html 파일 경로매핑
+//    }
 
     @RequestMapping("/board") //
     public String getBoardList(Model model){
