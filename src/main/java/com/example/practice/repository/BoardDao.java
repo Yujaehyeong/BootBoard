d308ffeaf8ed69ad36f9f95a59937636814c8411
Yujaehyeong <mynameisyjh@gmail.com>
2019-12-03 22:45:25 +0900

    postgresql db 연결

--- src/main/java/com/example/practice/repository/BoardDao.java
+++ src/main/java/com/example/practice/repository/BoardDao.java
@@ -1,6 +1,8 @@
 package com.example.practice.repository;
 
 import com.example.practice.vo.BoardVo;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,21 +11,12 @@ import java.util.List;
 @Repository
 public class BoardDao {
 
+    @Autowired
+    private SqlSession sqlSession;
 
     public List<BoardVo> selectBoardList() {
 
-        BoardVo board =  new BoardVo();
-        board.setNo(1L);
-        board.setTitle("게시판입니다.");
-        board.setContents("안녕하세요. 첫 게시판 입니다.");
-        board.setRegDate("2019-11-16");
-        board.setHit(111);
-        board.setUserName("홍길동");
-        board.setUserNo(1L);
-
-        List<BoardVo> boardList = new ArrayList<>();
-
-        boardList.add(board);
+        List<BoardVo> boardList = sqlSession.selectList("board.getBoardList");
 
         return boardList;
     }
