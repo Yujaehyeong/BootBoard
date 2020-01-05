69f9a5428172dabb65254c11538191847a2a130b
Yujaehyeong <mynameisyjh@gmail.com>
2020-01-05 23:33:55 +0900

    commit

--- src/main/java/com/example/practice/controller/UserController.java
+++ src/main/java/com/example/practice/controller/UserController.java
@@ -4,12 +4,9 @@ import com.example.practice.service.UserService;
 import com.example.practice.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
